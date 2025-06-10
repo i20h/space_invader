2bc581b06d98d3aabf0127e688cf8e57353ebd98
20h <tibodeplancke667@gmail.com>
2025-06-10 16:33:31 +0000

    feat: Add shooting mechanics to player class with bullet management

--- player.cpp
+++ player.cpp
@@ -18,9 +18,52 @@ void player::setPosition(const nsGraphics::Vec2D& delta) {
     nsGraphics::Vec2D position = entity::getPosition();
     nsGraphics::Vec2D newPosition = position + delta;
 
-    if (newPosition.getX() < limit.getX() || (newPosition.getX() + entity::computeSize().getX()) > limit.getY()) {
+    if (newPosition.getX() < limit.getX() || (newPosition.getX() + entity::computeSize().getX()) > limit.getY())
         return;
-    }
 
     entity::setPosition(newPosition);
 } // setPosition(const nsGraphics::Vec2D& delta, int direction)
+
+/**
+ * @brief Determines if the player is allowed to shoot a new bullet.
+ *
+ * This function checks two conditions:
+ * 1. At least 0.5 seconds have passed since the last bullet was shot.
+ * 2. The current number of bullets is less than or equal to the maximum allowed bullets.
+ *
+ * @return true if the player can shoot a new bullet, false otherwise.
+ */
+bool player::canShoot() const {
+    return (difftime(time(nullptr), lastBulletShot) >= 0.5 && (const int)bullets.size() <= maxBullets) ? true : false;
+} // canShoot()
+
+void player::shoot() {
+    if (!canShoot()) return;
+
+    lastBulletShot = time(nullptr);
+    bullets.emplace_back(getPosition() + nsGraphics::Vec2D(0, -10));
+} // shoot()
+
+void player::draw(MinGL& window) {
+    entity::draw(window);
+    removeOffscreenBullets();
+    for (auto& bullet : bullets) {
+        bullet.draw(window);
+        bullet.move();
+    }    
+} // draw(MinGL& window)
+
+bool player::isInvaderHit(const nsGui::Sprite& invaderSprite) {
+    for (auto it = bullets.begin(); it != bullets.end(); ++it) {
+        if (it->isColliding(invaderSprite)) {
+            bullets.erase(it);
+            return true;
+        }
+    }
+    return false;
+} // isInvaderHit(const nsGui::Sprite& invaderSprite) const
+
+void player::removeOffscreenBullets() {
+    bullets.erase(remove_if(bullets.begin(), bullets.end(),
+        [](const bullet& b) { return b.getPosition().getY() < 0; }), bullets.end());
+} // removeOffscreenBullets()
--- player.h
+++ player.h
@@ -1,15 +1,28 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <vector>
+#include <algorithm>
 #include "entity.h"
+#include "bullet.h"
+
+using namespace std;
 
 class player : public entity
 {
 private:
     nsGraphics::Vec2D limit;
+    time_t lastBulletShot = 0;
+    int maxBullets = 10;
+    vector<bullet> bullets;
 public:
     player(nsGraphics::Vec2D coords, nsGraphics::Vec2D limit = nsGraphics::Vec2D(0, 830));
     void setPosition(const nsGraphics::Vec2D& delta);
+    bool canShoot() const;
+    bool isInvaderHit(const nsGui::Sprite& invaderSprite);
+    void shoot();
+    void draw(MinGL& window) override;
+    void removeOffscreenBullets();
 };
 
 #endif // PLAYER_H
